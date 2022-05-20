037185943c7c00ff3e623b3e11363b2adf10996a
Sebastián Orozco <sebastianorozcogomez@gmail.com>
2022-05-20 04:14:45 -0500

    Created base style and structure

---
+++ src/routes/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from '../pages/Home'
+import { Login } from "../pages/Login";
+import '../styles/global.scss'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export { App };
