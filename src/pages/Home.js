0711103019768d52259402ec2bb6e80c0257cec0
Sebastián Orozco <sebastianorozcogomez@gmail.com>
2022-05-20 04:56:25 -0500

    Added home page

---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import { Button } from '../components/Button';
+import {ReactComponent as GuitarIllustration} from '../assets/playGuitar.svg'
+import '../styles/Home.scss'
+
+function Home() {
+    return (
+        <React.Fragment>
+            <Header>
+                <a className='navbar__menu__link' href='/login'>
+                    Ingresar
+                </a>
+                <a className='navbar__menu__link' href='/login'>
+                    Registrarse
+                </a>
+            </Header>
+            <main className='homePage'>
+                <div className='homePage__leftContainer'>
+                    <h1 className="homePage__leftContainer__title">AcaDemo</h1>
+                    <p className="homePage__leftContainer__text">La mayor comunidad educativa en español para aprender a tocar guitarra</p>
+                    <a href='/login'>
+                        <Button click="" className="btn--blue homePage__leftContainer__button">Comenzar</Button>
+                    </a>
+                </div>
+                <div className='homePage__rightContainer'>
+                    <GuitarIllustration className='guitarIllustration' />
+                </div>
+            </main>
+        </React.Fragment>
+    )
+}
+
+export { Home }
